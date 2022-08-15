2dd41c93b24f9c01c28c0d15827111a026742f42
Marcel Keller <mks.keller@gmail.com>
2022-08-15 05:01:27 +0000

    Fix compilation on ARM.

--- ref10/cpucycles.c
+++ ref10/cpucycles.c
@@ -3,9 +3,12 @@
 
 long long cpucycles_amd64cpuinfo(void)
 {
+#ifdef __x86_64__
   unsigned long long result;
   asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
     : "=a" (result) ::  "%rdx");
   return result;
+#else
+  return 0;
+#endif
 }
-
